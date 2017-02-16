f5325ad5511f495a582a0babde6eaca5828336f4
shaun.thium <shaun.thium@gmail.com>
2017-02-16 09:58:16 -0800

    Remove unused constant in secrets. Remove hardcoding of secrets path and assume that it's stored in the cwd instead

--- secrets/secrets.go
+++ secrets/secrets.go
@@ -3,6 +3,8 @@ package secrets
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 const (
@@ -11,16 +13,18 @@ const (
 	NetAppKey     = "netapp_key"
 
 	// Deploy
-	Instance1Hostname = "instance_1_hostname"
-	InstanceUsername  = "instance_username"
+	InstanceUsername = "instance_username"
 
 	PemFilePath = "pem_file_path"
-
-	// Hardcode path to secrets for now
-	secretsPath = "/Users/Shaun/go/src/github.com/shaunthium/honeydew/secrets/secrets.json"
 )
 
 func GetValueFromSecrets(key string) string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	// Assume that secrets are placed in the same "secrets" package
+	secretsPath := filepath.Join(cwd, "secrets", "secrets.json")
 	secrets, _ := ioutil.ReadFile(secretsPath)
 	data := make(map[string]string)
 	json.Unmarshal(secrets, &data)
